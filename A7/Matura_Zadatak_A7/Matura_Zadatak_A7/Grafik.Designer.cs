d09ff0c39f9c3946c02d0f0455b01dba1545c3da
Veljko Spasic <vex2005vex@gmail.com>
2022-11-17 12:01:10 +0100

    1

--- A7/Matura_Zadatak_A7/Matura_Zadatak_A7/Grafik.Designer.cs
+++ A7/Matura_Zadatak_A7/Matura_Zadatak_A7/Grafik.Designer.cs
@@ -28,11 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.checkedListBoxPredmeti = new System.Windows.Forms.CheckedListBox();
+            this.GV = new System.Windows.Forms.DataGridView();
+            this.btnPrikazi = new System.Windows.Forms.Button();
+            this.btnIzadji = new System.Windows.Forms.Button();
+            this.graph = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.GV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.graph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkedListBoxPredmeti
+            // 
+            this.checkedListBoxPredmeti.FormattingEnabled = true;
+            this.checkedListBoxPredmeti.Location = new System.Drawing.Point(22, 23);
+            this.checkedListBoxPredmeti.Name = "checkedListBoxPredmeti";
+            this.checkedListBoxPredmeti.Size = new System.Drawing.Size(219, 94);
+            this.checkedListBoxPredmeti.TabIndex = 0;
+            this.checkedListBoxPredmeti.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBoxPredmeti_ItemCheck);
+            // 
+            // GV
+            // 
+            this.GV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GV.Location = new System.Drawing.Point(22, 145);
+            this.GV.Name = "GV";
+            this.GV.Size = new System.Drawing.Size(219, 123);
+            this.GV.TabIndex = 1;
+            // 
+            // btnPrikazi
+            // 
+            this.btnPrikazi.Location = new System.Drawing.Point(22, 290);
+            this.btnPrikazi.Name = "btnPrikazi";
+            this.btnPrikazi.Size = new System.Drawing.Size(75, 23);
+            this.btnPrikazi.TabIndex = 2;
+            this.btnPrikazi.Text = "Prikazi";
+            this.btnPrikazi.UseVisualStyleBackColor = true;
+            this.btnPrikazi.Click += new System.EventHandler(this.btnPrikazi_Click);
+            // 
+            // btnIzadji
+            // 
+            this.btnIzadji.Location = new System.Drawing.Point(166, 290);
+            this.btnIzadji.Name = "btnIzadji";
+            this.btnIzadji.Size = new System.Drawing.Size(75, 23);
+            this.btnIzadji.TabIndex = 3;
+            this.btnIzadji.Text = "Izadji";
+            this.btnIzadji.UseVisualStyleBackColor = true;
+            this.btnIzadji.Click += new System.EventHandler(this.btnIzadji_Click);
+            // 
+            // graph
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.graph.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.graph.Legends.Add(legend1);
+            this.graph.Location = new System.Drawing.Point(265, 23);
+            this.graph.Name = "graph";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+            series1.IsValueShownAsLabel = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+            series2.IsValueShownAsLabel = true;
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+            series3.IsValueShownAsLabel = true;
+            series3.Legend = "Legend1";
+            series3.Name = "Series3";
+            this.graph.Series.Add(series1);
+            this.graph.Series.Add(series2);
+            this.graph.Series.Add(series3);
+            this.graph.Size = new System.Drawing.Size(300, 300);
+            this.graph.TabIndex = 4;
+            // 
+            // Grafik
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 377);
+            this.Controls.Add(this.graph);
+            this.Controls.Add(this.btnIzadji);
+            this.Controls.Add(this.btnPrikazi);
+            this.Controls.Add(this.GV);
+            this.Controls.Add(this.checkedListBoxPredmeti);
+            this.Name = "Grafik";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Grafik";
+            this.Load += new System.EventHandler(this.Grafik_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.graph)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBoxPredmeti;
+        private System.Windows.Forms.DataGridView GV;
+        private System.Windows.Forms.Button btnPrikazi;
+        private System.Windows.Forms.Button btnIzadji;
+        private System.Windows.Forms.DataVisualization.Charting.Chart graph;
     }
 }
--- A7/Matura_Zadatak_A7/Matura_Zadatak_A7/Grafik.cs
+++ A7/Matura_Zadatak_A7/Matura_Zadatak_A7/Grafik.cs
@@ -7,14 +7,123 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
 
 namespace Matura_Zadatak_A7
 {
     public partial class Grafik : Form
     {
+
+        SqlConnection conn = new SqlConnection(@"Data Source=192.168.0.20;Initial Catalog=A7;User ID=SA;Password=sp@sic123.");
+
         public Grafik()
         {
             InitializeComponent();
+        }
+
+        private void Grafik_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(@"SELECT predmet FROM Predmet;", conn);
+                if (adapter != null) 
+                {
+                    DataTable predmeti = new DataTable();
+                    string predmet = "";
+                    adapter.Fill(predmeti);
+                    foreach (DataRow row in predmeti.Rows) 
+                    {
+                        predmet = row[0].ToString();
+                        checkedListBoxPredmeti.Items.Add(predmet,CheckState.Unchecked);
+                    }
+
+                    graph.Series[0].LegendText = (DateTime.Now.Year - 3).ToString();
+                    graph.Series[0].LegendText = (DateTime.Now.Year - 2).ToString();
+                    graph.Series[0].LegendText = (DateTime.Now.Year - 1).ToString();
+                }
+            }
+           
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            finally 
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnPrikazi_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                conn.Open();
+                GV.DataSource = null;
+                GV.Refresh();
+                graph.Series.Clear();
+                string predmeti = "";
+                SqlCommand comm = new SqlCommand(@"
+                                                SELECT predmet,
+                                                (SELECT COUNT(studentID) 
+                                                FROM  Izabrani_Predmet AS IZP JOIN Predmet AS P 
+                                                ON IZP.predmetID = P.predmetID 
+                                                WHERE godina_slusanja = '2019' AND P.predmet IN (@predmet)) AS '2019',
+                                                (SELECT COUNT(studentID) FROM  
+                                                Izabrani_Predmet AS IZP JOIN Predmet AS P 
+                                                ON IZP.predmetID = P.predmetID 
+                                                WHERE godina_slusanja = '2018' AND P.predmet IN (@predmet)) AS '2018',
+                                                (SELECT COUNT(studentID) FROM  
+                                                Izabrani_Predmet AS IZP JOIN Predmet AS P 
+                                                ON IZP.predmetID = P.predmetID  
+                                                WHERE godina_slusanja = '2017' AND P.predmet IN (@predmet)) AS '2017'
+                                                FROM Predmet
+                                                WHERE predmet IN (@predmet)
+                                                GROUP BY predmet", conn);
+                comm.Parameters.AddWithValue("@predmet", predmeti);
+                SqlDataAdapter adapter = new SqlDataAdapter (comm);
+                if (adapter != null) 
+                {
+                    DataTable GV_tabela = new DataTable();
+                    adapter.Fill(GV_tabela);
+                    GV.DataSource = GV_tabela;
+                    GV.Refresh();
+
+                    
+
+                }
+                
+            }
+
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            finally 
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnIzadji_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void checkedListBoxPredmeti_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (checkedListBoxPredmeti.CheckedItems.Count > 4) 
+            {
+                /*for (int i = 0; i < checkedListBoxPredmeti.Items.Count; i++) 
+                {
+                    checkedListBoxPredmeti.SetItemChecked(i, false);
+                }*/
+                    
+                MessageBox.Show("Mozete pregledati najvise 5 predmeta po prikazu!", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
