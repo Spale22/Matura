b5b814c27c68a0ebd4f99c28ed2b4608219869d7
Veljko Spasic <vex2005vex@gmail.com>
2022-11-20 18:10:52 +0100

    1

--- A8/Matura_Zadatak_A8/Matura_Zadatak_A8/Program.cs
+++ A8/Matura_Zadatak_A8/Matura_Zadatak_A8/Program.cs
@@ -16,7 +16,7 @@ namespace Matura_Zadatak_A8
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Antikviteti_Lokacija());
         }
     }
 }
