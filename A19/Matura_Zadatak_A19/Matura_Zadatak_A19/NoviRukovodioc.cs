cc912fe57949d04303dcc471abd4ae66947bec55
Veljko Spasic <vex2005vex@gmail.com>
2022-12-15 22:17:31 +0100

    1

---
+++ A19/Matura_Zadatak_A19/Matura_Zadatak_A19/NoviRukovodioc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matura_Zadatak_A19
+{
+    public partial class NoviRukovodioc : Form
+    {
+        public NoviRukovodioc()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- A19/Matura_Zadatak_A19/Matura_Zadatak_A19/Properties/AssemblyInfo.cs
+++ A19/Matura_Zadatak_A19/Matura_Zadatak_A19/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Matura_zadatak_A1")]
+[assembly: AssemblyTitle("Matura_Zadatak_A19")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Matura_zadatak_A1")]
+[assembly: AssemblyProduct("Matura_Zadatak_A19")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b09d9a76-fa90-4988-84e3-68cac4e3d77e")]
+[assembly: Guid("fb0caf24-f649-4a5f-b79d-8d6227a8c2ce")]
 
 // Version information for an assembly consists of the following four values:
 //
